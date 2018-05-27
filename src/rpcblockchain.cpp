b4a638c111ae03b37c898451311715f9dfd4b366
Thomas Le <tvle83@gmail.com>
2018-05-27 01:19:18 -0500

    adds mint, knreward,knrewardpercent and staker payment to rpc response

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -15,6 +15,8 @@
 
 #include "json/json_spirit_value.h"
 
+#include "karmanodeman.h"
+
 using namespace json_spirit;
 using namespace std;
 
@@ -53,6 +55,11 @@ double GetDifficulty(const CBlockIndex* blockindex)
 Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails = false)
 {
     Object result;
+
+    int64_t MNPayment = GetKarmanodePayment(blockindex->nHeight, GetBlockValue(blockindex->nHeight));
+    int64_t StakerPayment = blockindex->nMint - MNPayment;
+    double MNRewardPercent = floor(((double)MNPayment / (double)blockindex->nMint) * 100);
+
     result.push_back(Pair("hash", block.GetHash().GetHex()));
     int confirmations = -1;
     // Only report confirmations if the block is on the main chain
@@ -84,6 +91,12 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDe
     CBlockIndex* pnext = chainActive.Next(blockindex);
     if (pnext)
         result.push_back(Pair("nextblockhash", pnext->GetBlockHash().GetHex()));
+
+    result.push_back(Pair("mint", ValueFromAmount(GetBlockValue(blockindex->nHeight))));
+    result.push_back(Pair("knReward", ValueFromAmount(MNPayment)));
+    result.push_back(Pair("knRewardPercent", MNRewardPercent));
+    result.push_back(Pair("stakerPayment", ValueFromAmount(StakerPayment)));
+
     return result;
 }
 
