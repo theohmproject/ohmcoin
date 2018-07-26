34be49b7612ad059bfd5be3aa47585d4f133c364
Andrew LaChasse <rasalghul@ohmcoin.org>
2018-07-26 13:50:53 -0700

    Update Copyright and added back PIVX Copyright

--- src/qt/ohmc.cpp
+++ src/qt/ohmc.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
-// Copyright (c) 2015-2017 The OHMC developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2017-2018 The OHMC 
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
