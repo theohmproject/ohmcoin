8f617e04b53bd5f83e09d5e44441c8feea37366f
2a5A1Ghu1 <13294167+2a5A1Ghu1@users.noreply.github.com>
2020-10-01 00:59:03 -0700

    [Build] Remove unnecessary BOOST dependency
    https://github.com/PIVX-Project/PIVX/pull/605

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -5247,11 +5247,10 @@ void CWallet::ReconsiderZerocoins(std::list<CZerocoinMint>& listMintsRestored, s
 
 string CWallet::GetUniqueWalletBackupName(bool fzohmcAuto) const
 {
-    posix_time::ptime timeLocal = posix_time::second_clock::local_time();
     stringstream ssDateTime;
+    std::string strWalletBackupName = strprintf("%s", DateTimeStrFormat(".%Y-%m-%d-%H-%M", GetTime()));
+    ssDateTime << strWalletBackupName;
 
-
-    ssDateTime << gregorian::to_iso_extended_string(timeLocal.date()) << "-" << timeLocal.time_of_day();
     return strprintf("wallet%s.dat%s", fzohmcAuto ? "-autozohmcbackup" : "", DateTimeStrFormat(".%Y-%m-%d-%H-%M", GetTime()));
 }
 
