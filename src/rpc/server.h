43f49af53e6968b170d4d600393873a9da812f88
2a5A1Ghu1 <13294167+2a5A1Ghu1@users.noreply.github.com>
2020-10-08 03:29:47 -0700

    Add getaccumulatorvalues RPC
    Return the accumulator values associated with a particular block height.
    
    https://github.com/PIVX-Project/PIVX/commit/8a6d425672886dd82021d1c9f0dff61fe50441f8

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -294,6 +294,7 @@ extern UniValue verifychain(const UniValue& params, bool fHelp);
 extern UniValue getchaintips(const UniValue& params, bool fHelp);
 extern UniValue invalidateblock(const UniValue& params, bool fHelp);
 extern UniValue reconsiderblock(const UniValue& params, bool fHelp);
+extern UniValue getaccumulatorvalues(const UniValue& params, bool fHelp);
 
 extern UniValue obfuscation(const UniValue& params, bool fHelp); // in rpc/karmanode.cpp
 extern UniValue getpoolinfo(const UniValue& params, bool fHelp);
