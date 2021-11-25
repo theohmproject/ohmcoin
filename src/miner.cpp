222b8a5c6c06ece7f59522e18bd1035220d528d9
wqking <wqking@outlook.com>
2021-11-25 11:11:12 +0800

    Fixed compile errors

--- src/miner.cpp
+++ src/miner.cpp
@@ -606,6 +606,9 @@ bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
 }
 
 bool fGenerateBitcoins = false;
+bool fMintableCoins = false;
+int nMintableLastCheck = 0;
+
 
 // ***TODO*** that part changed in bitcoin, we are using a mix with old one here for now
 
